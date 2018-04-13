ee2bdd0972ea64ac82d94a80d6b5e5565130dca3
hegang0213 <helleye@126.com>
2018-04-13 16:05:23 +0800

    no

--- app/src/main/java/com/bdwater/waterservice/MainActivity.java
+++ app/src/main/java/com/bdwater/waterservice/MainActivity.java
@@ -1,13 +1,28 @@
 package com.bdwater.waterservice;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import it.sephiroth.android.library.bottomnavigation.BottomNavigation;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.viewPager)
+    ViewPager viewPager;
+    @BindView(R.id.bottomNavigation)
+    BottomNavigation bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+
     }
 }
---
+++ app/src/main/java/com/bdwater/waterservice/main/PagerAdapter.java
@@ -0,0 +1,25 @@
+package com.bdwater.waterservice.main;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by hegang on 18/4/13.
+ */
+
+public class PagerAdapter extends FragmentPagerAdapter {
+    public PagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
