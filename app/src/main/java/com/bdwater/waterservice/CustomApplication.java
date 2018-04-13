a746cb93e30926ad9bab44f6dc0515073b29abca
hegang0213 <helleye@126.com>
2018-04-13 16:08:12 +0800

    no

---
+++ app/src/main/java/com/bdwater/waterservice/CustomApplication.java
@@ -0,0 +1,11 @@
+package com.bdwater.waterservice;
+
+import android.app.Application;
+
+/**
+ * Created by hegang on 18/4/13.
+ */
+
+public class CustomApplication extends Application {
+
+}
--- app/src/main/java/com/bdwater/waterservice/main/PagerAdapter.java
+++ app/src/main/java/com/bdwater/waterservice/main/PagerAdapter.java
@@ -20,6 +20,6 @@ public class PagerAdapter extends FragmentPagerAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return 4;
     }
 }
