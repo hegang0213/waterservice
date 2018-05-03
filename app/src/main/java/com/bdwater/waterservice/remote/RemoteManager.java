e3851fd7ef9bd3d0ec4d87b7262ed270a8ee123e
hegang0213 <helleye@126.com>
2018-05-03 16:36:58 +0800

    no

---
+++ app/src/main/java/com/bdwater/waterservice/remote/RemoteManager.java
@@ -0,0 +1,44 @@
+package com.bdwater.waterservice.remote;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * Created by hegang on 18/5/2.
+ */
+
+public class RemoteManager {
+    private final static String baseUrl = "http://222.222.178.137/";
+    private OkHttpClient client;
+    private Request.Builder requestBuilder;
+    public RemoteManager() {
+        this.client = new OkHttpClient();
+        this.requestBuilder = new Request.Builder().url(baseUrl);
+    }
+    public void execute(RequestBody requestBody) {
+         Request request = this.requestBuilder.post(requestBody).build();
+         execute(request);
+    }
+    public void execute(Request request) {
+        Call call = this.client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+
+            }
+        });
+    }
+
+}
