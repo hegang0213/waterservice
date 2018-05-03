ab140d231eed9350efa3de653de8b3a345632528
hegang <helleye@126.com>
2018-05-03 16:50:36 +0800

    home change

---
+++ app/src/main/java/com/bdwater/waterservice/model/User.java
@@ -0,0 +1,12 @@
+package com.bdwater.waterservice.model;
+
+/**
+ * Created by hegang on 2018/5/3.
+ */
+
+public class User {
+    public boolean isLogon = false;
+    public String tel = "18003328885";
+    public Integer customerNo = 30247;
+    public static User instance = new User();
+}
--- app/src/main/java/com/bdwater/waterservice/query/QueryFragment.java
+++ app/src/main/java/com/bdwater/waterservice/query/QueryFragment.java
@@ -6,9 +6,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.widget.ProgressBar;
 
 import com.bdwater.waterservice.R;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshFooter;
+import com.scwang.smartrefresh.layout.api.RefreshHeader;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+import com.scwang.smartrefresh.layout.constant.RefreshState;
+import com.scwang.smartrefresh.layout.listener.OnMultiPurposeListener;
+import com.scwang.smartrefresh.layout.listener.OnRefreshListener;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,6 +26,10 @@ import butterknife.ButterKnife;
  * A simple {@link Fragment} subclass.
  */
 public class QueryFragment extends Fragment {
+    @BindView(R.id.refreshLayout)
+    SmartRefreshLayout refreshLayout;
+    @BindView(R.id.progressBar)
+    ProgressBar progressBar;
     @BindView(R.id.webView)
     WebView webView;
 
@@ -34,7 +47,30 @@ public class QueryFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_query, container, false);
         ButterKnife.bind(this, view);
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                progressBar.setProgress(newProgress);
+                if(newProgress == 100) {
+                    refreshLayout.finishRefresh();
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+        refreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(RefreshLayout refreshLayout) {
+                reload();
+            }
+        });
+        this.reload();
         return view;
+    }
+    private void reload() {
+        this.webView.loadUrl("http://222.223.34.130:5151/");
+        this.progressBar.setProgress(0);
+        this.progressBar.setVisibility(View.VISIBLE);
     }
 
 }
